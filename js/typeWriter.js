a2bdd1e38ecfa2800fa008d10f26818fb091880e
Kalyan Sambhana <kalyansambhana@Kalyans-MacBook-Air.local>
2023-02-19 13:58:03 +0530

    first version

---
+++ js/typeWriter.js
@@ -0,0 +1,37 @@
+
+let i = 0;
+let text1 = "Hello! Miss Sushma.";
+let text2 = "Happy 6th Anniversary."
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).style.fontSize = "xx-large";
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
